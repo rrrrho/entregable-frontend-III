e56eeb47d863c52d36b6de12952a23eeba21da8c
rrrrho <rocioghillino@gmail.com>
2022-09-02 15:44:06 -0300

    agrego trabajo terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// El componente App es el padre de:
+// - Cabecera
+// - Listado
+// ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
+// MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
+// PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import { useState } from 'react';
+import Cabecera from './components/Cabecera.js';
+import Listado from './components/Listado.js';
+
 
 function App() {
+
+  let [carrito, setCarrito] = useState(0);
+
+  function aumentarCarrito() {
+    setCarrito(carrito+1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera cantidad={carrito}/>
+      <Listado onComprar={aumentarCarrito}/>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+export default function Item(props) {
+
+  let [stock, setStock] = useState(props.stock)
+
+  return (
+    <div className='producto'>
+      <h3>{props.nombre}</h3>
+      <p>{props.descripcion}</p>
+      <h5>En stock: {stock > 0 ? stock : <span>agotado</span>}</h5>
+      <button type="button" onClick={() => {setStock(stock-1); props.onComprar()}} disabled={stock <= 0 ? true : false}>{stock <= 0 ? 'SIN STOCK' : 'COMPRAR'}</button>
+    </div>
+  )
+}
